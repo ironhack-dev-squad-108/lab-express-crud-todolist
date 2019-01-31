e710100c0d02f0d8f4a048bc7f7ab9030dfdc2aa
Goldziher <nhirschfeld@gmail.com>
2019-01-31 18:35:08 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -26,10 +26,47 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/delete/:id', (req, res, next) => {
-  Match.deleteOne({"_id": req.params.id})
-  .then(() => res.redirect("/"))
-  .catch((err) => console.log("An error has occured: " + err))
-})  
+  Match.deleteOne({
+      "_id": req.params.id
+    })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log("An error has occured: " + err))
+})
+
+router.get('/ranking', (req, res, next) => {
+  Match.find()
+    .then(data => {
+      let players = [];
+      let scores = [];
+      for (var i of data) {
+        players.push(i.player1);
+        players.push(i.player2);
+        scores.push(i.score1);
+        scores.push(i.score2)
+      }
+      let results = scores.map((v) => {
+        if (v === 0) {
+          return "Lose"
+        } else if (v === 1) {
+          return "Draw"
+        } else {
+          return "Win"
+        }
+      })
+      let playersScores = []
+      for (let i = 0; i<players.length; i++) {
+        var temp = {
+          player: players[i],
+          score: scores[i],
+          result: results[i],
+        }
+        playersScores.push(temp)
+      }
+      playersScores = playersScores.sort((a,b) => b.score - a.score);
+      res.render('ranking', { playersScores })
+    })
+    .catch((err) => console.log("An error has occured: " + err))
+})
 
 router.post("/addmatch", (req, res, next) => {
   const {
