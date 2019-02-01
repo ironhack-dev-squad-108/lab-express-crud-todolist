9837bdc31a2c34d8b2ded3f84e0222d14fdc4492
André Sebastian <a.sebastian@posteo.de>
2019-02-01 10:56:57 +0100

    First commit, done

---
+++ models/Match.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const matchSchema = new Schema({
+  sport: String,
+  player1: String,
+  player2: String,
+  score1: Number,
+  score2: Number
+});
+
+const Match = mongoose.model("Match", matchSchema);
+
+module.exports = Match;
