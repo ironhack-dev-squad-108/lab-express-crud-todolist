d1e03522537bc6047f5c25652ad615d54ce004db
Malte <m.felmy@googlemail.com>
2019-02-01 10:54:09 +0100

    Added ranking

--- ironmatch/routes/index.js
+++ ironmatch/routes/index.js
@@ -4,6 +4,18 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  console.log("TCL: req.query.search", req.query.search);
+  if (req.query.search) {
+    Match.find({
+      $or: [{ player1: { $eq: req.query.search } }, { player2: { $eq: req.query.search } }]
+    })
+      .then(docs => {
+        res.render("index", {
+          matches: docs
+        });
+      })
+      .catch(console.log);
+  }
   Match.find()
     .then(docs => {
       res.render("index", {
@@ -13,6 +25,7 @@ router.get("/", (req, res, next) => {
     .catch(console.log);
 });
 
+// add new match to database
 router.post("/", (req, res, next) => {
   console.log("TCL: req.body", req.body);
   const { player1, score1, score2, player2 } = req.body;
@@ -28,9 +41,46 @@ router.post("/", (req, res, next) => {
     });
 });
 
-router.get("/:id", (req, res) => {
+// delete match
+router.get("/delete/:id", (req, res) => {
   Match.findByIdAndDelete(req.params.id)
     .then(() => res.redirect("/"))
+    .catch(console.log);
+});
+
+router.get("/ranking", (req, res) => {
+  Match.find()
+    .then(docs => {
+      let ranking = {};
+      for (let i = 0; i < docs.length; i++) {
+        if (!ranking.hasOwnProperty(docs[i].player1)) {
+          ranking[docs[i].player1] = 0;
+        }
+        if (!ranking.hasOwnProperty(docs[i].player2)) {
+          ranking[docs[i].player2] = 0;
+        }
+        if (docs[i].score1 > docs[i].score2) {
+          ranking[docs[i].player1] += 2;
+        } else if (docs[i].score1 < docs[i].score2) {
+          ranking[docs[i].player2] += 2;
+        } else {
+          ranking[docs[i].player1] += 1;
+          ranking[docs[i].player2] += 1;
+        }
+      }
+      // res.send(ranking);
+      let keys = Object.keys(ranking);
+      let values = Object.values(ranking);
+      let arr = [];
+      for (let i = 0; i < keys.length; i++) {
+        arr.push([keys[i], values[i]]);
+      }
+      arr.sort((a, b) => b[1] - a[1]);
+      // res.send(arr);
+      res.render("ranking", {
+        ranking: arr
+      });
+    })
     .catch(console.log);
 });
 
